5234d70cfde92f5d49184d91e37e3dc3b45b5469
Krishna Suthar <krishnasuthar568@gmail.com>
2026-06-30 23:11:24 +0530

    configure intents in app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 require('dotenv').config();
 
-// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, ] });
+// Create a new client instance. client - connection manager between the application and Discord.
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
+
 client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log in to Discord with your client's token
+/**
+ * - Log in to Discord with your client's token - bot becomes online
+ * - TCP/WebSocket connection is established
+ * - Heartbeat/Heartbeat loop ACK starts here
+ */
 client.login(process.env.TOKEN);
