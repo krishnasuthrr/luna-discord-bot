5039bb13233cb4f4dd119a760c970ec5c09180dd
Krishna Suthar <krishnasuthar568@gmail.com>
2026-07-03 23:46:35 +0530

    dynamic command imports in app.js using fs and path modules and Collection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+const fs = require("node:fs");
+const path = require("node:path");
 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 
 // Create a new client instance. client - connection manager between the application and Discord.
 const client = new Client({
@@ -17,19 +19,29 @@ client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// listen to events
+client.commands = new Collection()
 
-client.on(Events.MessageCreate, (message) => {
-  if (message.author.bot) return
-  message.react('❤️') 
-}); 
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
 
-client.on(Events.MessageDelete, async (message) => {
-  console.log(message)
-  if (!message.author) return;
-  if (message.author.bot) return;
-  await message.channel.send(`${message.author} I saw that`)
-})
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+      );
+    }
+  }
+}
 
 /**
  * - Log in to Discord with your client's token - bot becomes online
