fcc3ffdac45f5d1934da7bc78d3b05fff20b81de
Krishna Suthar <krishnasuthar568@gmail.com>
2026-07-06 21:55:33 +0530

    add Interaction event listener in app.js

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@ for (const folder of commandFolders) {
     // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ("data" in command && "execute" in command) {
       client.commands.set(command.data.name, command);
+      // console.log(command.data.toJSON())
     } else {
       console.log(
         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
@@ -42,6 +43,40 @@ for (const folder of commandFolders) {
     }
   }
 }
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  // console.log(interaction) 
+
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+
+    await command.execute(interaction);
+
+  } catch (error) {
+    console.error(error);
+
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        // eslint-disable-next-line no-undef
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        // eslint-disable-next-line no-undef
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
+});
 
 /**
  * - Log in to Discord with your client's token - bot becomes online
